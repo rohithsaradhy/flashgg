d614c6f7864ea8e0a9dc255bf148515ba8b7c239
rohithsaradhy <rohithsaradhy@gmail.com>
2022-08-24 19:39:16 +0200

    Cleaned up Directory, separated WH & ZH; Needs some testing

--- Systematics/test/scripts_tags_2018RR/script_zh_sig_all_mass.sh
+++ Systematics/test/scripts_tags_2018RR/script_zh_sig_all_mass.sh
@@ -1,10 +1,10 @@
 nEvents=-1
-outdir=sig_all_mass
-dumper=../WH_anom_dumper.py
+outdir=zh_sig_all_mass
+dumper=../ZH_anom_dumper.py
 queue=tomorrow
-json=json/sig_all_mass.json
+json=json/zh_sig_all_mass.json
 
-date_name='2022_08_23_STXS_Cuts'
+date_name='2022_08_24_ZH_NoCuts'
 eos_dir=/eos/user/r/rsaradhy/vh_anomalous/$date_name/raw/$outdir/
 mkdir -p $eos_dir
 fggRunJobs.py --load $json \
