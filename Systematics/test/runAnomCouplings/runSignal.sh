de94b729ec5a94cdd4d6e9ad89251e6c5e3609bc
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-10-10 09:49:46 +0200

    modify submission jsons for 2016 and 2017 datasets

--- Systematics/test/runAnomCouplings/runSignal.sh
+++ Systematics/test/runAnomCouplings/runSignal.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_29052022"
+storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_07102022"
 outDir=$1
 if [[ -z $outDir ]]; then
     echo "usage: $0 <outDir>"
     exit -1
 fi
+year2016preVFPDir="signal_IA_UL16preVFP"
+year2016postVFPDir="signal_IA_UL16postVFP"
+year2017Dir="signal_IA_UL17"
+year2018Dir="signal_IA_UL18"
 
-mkdir -p ${storageDir}/${outDir}_signal_IA_16/
-fggRunJobs.py --load sig_jobs_2016.json -d ${outDir}_signal_IA_16 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_signal_IA_16/ -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
+mkdir -p ${storageDir}/${outDir}_${year2016preVFPDir}
+fggRunJobs.py --load sig_jobs_2016preVFP_UL.json -d ${outDir}_${year2016preVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016preVFPDir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
 
-mkdir -p ${storageDir}/${outDir}_signal_IA_17/
-fggRunJobs.py --load sig_jobs_2017.json -d ${outDir}_signal_IA_17 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_signal_IA_17/ -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
+#mkdir -p ${storageDir}/${outDir}_${year2016postVFPDir}
+#fggRunJobs.py --load sig_jobs_2016postVFP_UL.json -d ${outDir}_${year2016postVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016postVFPDir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
 
-mkdir -p ${storageDir}/${outDir}_signal_IA_18/
-fggRunJobs.py --load sig_jobs_2018.json -d ${outDir}_signal_IA_18 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_signal_IA_18/ -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
+#mkdir -p ${storageDir}/${outDir}_${year2017Dir}
+#fggRunJobs.py --load sig_jobs_2017.json -d ${outDir}_${year2017Dir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2017Dir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
+
+#mkdir -p ${storageDir}/${outDir}_${year2018Dir}
+#fggRunJobs.py --load sig_jobs_2018.json -d ${outDir}_${year2018Dir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=False doPdfWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2018Dir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
