5917a53e7f6fe49c20de9354e66bd6cfeeff9379
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-10-11 14:29:41 +0200

    simplify submission shell files

--- Systematics/test/runAnomCouplings/runData.sh
+++ Systematics/test/runAnomCouplings/runData.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_29052022"
+storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_07102022"
 outDir=$1
 if [[ -z $outDir ]]; then
     echo "usage: $0 <outDir>"
     exit -1
 fi
+year2016preVFPDir="data_IA_UL16preVFP"
+year2016postVFPDir="data_IA_UL16postVFP"
+year2017Dir="data_IA_UL17"
+year2018Dir="data_IA_UL18"
 
-mkdir -p ${storageDir}/${outDir}_data_16/
-fggRunJobs.py --load data_jobs_2016.json -d ${outDir}_data_16 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_data_16/ -q workday -n 500 -H --no-copy-proxy &
+mkdir -p ${storageDir}/${outDir}_${year2016preVFPDir}
+fggRunJobs.py --load data_jobs_2016preVFP_UL.json -d ${outDir}_${year2016preVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016preVFPDir} -q workday -n 500 -H --no-copy-proxy &
 
-mkdir -p ${storageDir}/${outDir}_data_17/
-fggRunJobs.py --load data_jobs_2017.json -d ${outDir}_data_17 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False  --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_data_17/ -q workday -n 500 -H --no-copy-proxy &
+mkdir -p ${storageDir}/${outDir}_${year2016postVFPDir}
+fggRunJobs.py --load data_jobs_2016postVFP_UL.json -d ${outDir}_${year2016postVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016postVFPDir} -q workday -n 500 -H --no-copy-proxy &
 
-mkdir -p ${storageDir}/${outDir}_data_18/
-fggRunJobs.py --load data_jobs_2018.json -d ${outDir}_data_18 -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False  --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_data_18/ -q workday -n 500 -H --no-copy-proxy &
+#mkdir -p ${storageDir}/${outDir}_${year2017Dir}
+#fggRunJobs.py --load data_jobs_2017_UL.json -d ${outDir}_${year2017Dir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False  --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2017Dir} -q workday -n 500 -H --no-copy-proxy &
+
+#mkdir -p ${storageDir}/${outDir}_${year2018Dir}
+#    fggRunJobs.py --load data_jobs_2018_UL.json -d ${outDir}_${year2018Dir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpWorkspace=True dumpTrees=True doSystematics=False applyNNLOPSweight=False recalculatePDFWeights=False vbfTagsOnly=True dumpLHE=False melaEFT=False  --nCondorCpu=2 --no-use-tarball pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2018Dir} -q workday -n 500 -H --no-copy-proxy &
