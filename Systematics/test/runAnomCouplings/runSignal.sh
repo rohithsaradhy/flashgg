c8000a776a0ea7c21bee0fb5195a2ee276bebe6a
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2023-01-31 13:31:07 +0100

    uncomment the 2016preVFP: redo it with full systematics

--- Systematics/test/runAnomCouplings/runSignal.sh
+++ Systematics/test/runAnomCouplings/runSignal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_2023_01_27_allSTXSCats"
+storageDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/vbfhgg/HiggsCouplings/Trees_2023_01_31_allSTXSCats"
 outDir=$1
 if [[ -z $outDir ]]; then
     echo "usage: $0 <outDir>"
@@ -11,8 +11,8 @@ year2016postVFPDir="signal_IA_UL16postVFP"
 year2017Dir="signal_IA_UL17"
 year2018Dir="signal_IA_UL18"
 
-#mkdir -p ${storageDir}/${outDir}_${year2016preVFPDir}
-#fggRunJobs.py --load sig_jobs_2016preVFP_UL.json -d ${outDir}_${year2016preVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=True doPdfWeights=True useParentDataset=True anomalousCouplings=True dumpLHE=False melaEFT=True --nCondorCpu=5 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016preVFPDir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
+mkdir -p ${storageDir}/${outDir}_${year2016preVFPDir}
+fggRunJobs.py --load sig_jobs_2016preVFP_UL.json -d ${outDir}_${year2016preVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=True doPdfWeights=True useParentDataset=True anomalousCouplings=True dumpLHE=False melaEFT=True --nCondorCpu=5 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016preVFPDir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
 
 mkdir -p ${storageDir}/${outDir}_${year2016postVFPDir}
 fggRunJobs.py --load sig_jobs_2016postVFP_UL.json -d ${outDir}_${year2016postVFPDir} -x cmsRun ../workspaceVbf.py metaDataSrc=flashgg dumpTrees=True doSystematics=True applyNNLOPSweight=True recalculatePDFWeights=True doPdfWeights=True useParentDataset=True anomalousCouplings=True dumpLHE=False melaEFT=True --nCondorCpu=5 pujidWP=tight maxEvents=-1 --stage-to=${storageDir}/${outDir}_${year2016postVFPDir} -q workday -n 100 -H --no-copy-proxy --no-use-tarball &
